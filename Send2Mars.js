471e98c6fea8f4578f8de79499766841c8300c2e
Jeremy Anderson <janderson@janderson-mbp.local>
2015-12-17 14:23:09 -0500

    add new files

---
+++ Send2Mars.js
@@ -0,0 +1,17 @@
+/*
+ here's how this needs to work
+
+ - get the location of the current page
+ - load contents of MARS window via js?
+ - update text of IOS form and set UI hide css for that ui component
+ - click upload
+
+ ALTERNATIVELY
+ - a chrome extension
+ - inject a js into the MARS app
+    * search itunes field
+    * open new window/tab with search results
+    * change input to grabID button after search
+    * grabID gets the URL of the related iTunes page
+
+ */
---
+++ content.js
@@ -0,0 +1,3 @@
+var modal = $('#upload_choice_modal').attr('class');
+
+console.log(modal);
